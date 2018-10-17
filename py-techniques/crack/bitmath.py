b638869a07f90fbb7bb96dcbac4027fe22daf3f3
rrguzman1976 <35820508+rrguzman1976@users.noreply.github.com>
2018-10-17 16:48:45 -0400

    Commit

---
+++ py-techniques/crack/bitmath.py
@@ -0,0 +1,33 @@
+class bitmath(object):
+    """description of class"""
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def getBit(n, ith):
+        return (n & (1 << ith)) != 0
+
+    @staticmethod
+    def setBit(n, ith):
+        return (n | (1 << ith))
+
+    @staticmethod
+    def clearBit(n, ith):
+        return (n & ~(1 << ith))
+
+    @staticmethod
+    def clearMSBBits(n, ith):
+        return (n & ((1 << ith) - 1))
+
+    @staticmethod
+    def clearLSBBits(n, ith):
+        return (n & (-1 << (ith + 1)))
+
+    @staticmethod
+    def updateBit(n, ith, val):
+        ''' val should be True or False. '''
+        eraseBit = bitmath.clearBit(n, ith)
+        newBit = (1 if val else 0) << ith
+        return eraseBit | newBit
+
