24e9210ccce3f6e6079cab65616f7016eeb05d1c
rrguzman1976 <35820508+rrguzman1976@users.noreply.github.com>
2018-10-24 22:15:16 -0400

    wed

--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -1,41 +1,40 @@
-from collections import defaultdict
+from itertools import combinations
+from collections import defaultdict, Counter
 
-def contacts(queries):
-    #book = []
-    results = []
-    index = defaultdict(list)
+# Complete the countTriplets function below.
+def countTriplets(arr, r):
+    triples = 0
+    left = defaultdict(int)
+    right = Counter(arr)
     
-    for q in queries:
-        cmd, name = q[0], q[1]
-        #print(cmd, name)
-        if cmd == 'add':
-            if name[0] in index:
-                index[name[0]].append(name)
-            else:
-                index[name[0]] = [name]
-                
-            #book.append(name)
-        elif cmd == 'find':
-            lookup = index[name[0]]
-            found = [n for n in lookup if n.startswith(name)]
-            results.append(len(found))
-        else:
-            raise Exception('Invalid command!')
-    #print(index)       
-    return results
+    # left = current/r, right = current * r
+    for n in arr:
 
+        #print('L{0}: {1}'.format(n, left))
+        #print('R{0}: {1}'.format(n, right))
+
+        # Decrement right (in use)
+        right[n] -= 1
+
+        if n%r == 0:
+            n3 = right[n*r]
+            n1 = left[n//r]
+            triples += n1*n3
+
+        left[n] += 1
+        
+    return triples
 
 # File harness
 try:
 
-    with open("C:\\Users\\ricardogu\\Desktop\\test.txt", "r", encoding="utf-8") as f:
-        queries = []
-        for _ in range(int(f.readline().rstrip())):
+    with open("C:\\Users\\rguzman\\Desktop\\test.txt", "r", encoding="utf-8") as f:
+        n, r = list(map(int, f.readline().rstrip().split()))
 
-            queries.append(f.readline().rstrip().split())
+        nums = list(map(int, f.readline().rstrip().split()))
     
-    #print(queries)
-    results = contacts(queries)
+    results = countTriplets(nums, r)
     print(results)
+
 except Exception as e:
     print(e)
