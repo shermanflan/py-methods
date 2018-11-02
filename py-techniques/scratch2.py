9bda2c351cd544e16db321af27d608de3c91bf42
Ricardo Guzman <rrguzman1976@hotmail.com>
2018-11-02 11:54:19 -0400

    fri

--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -1,24 +1,18 @@
-from itertools import permutations, combinations
+# File harness
+try:
 
-def separateNumbers(s):
-    # Complete this function
-    if s[0] == s:
-        print('NO')
-        return
+    with open("C:\\Users\\ricardogu\\Desktop\\test.txt", "r", encoding="utf-8") as f: #home
+    #with open("C:\\Users\\rguzman\\Desktop\\test.txt", "r", encoding="utf-8") as f: #work
+        n = int(f.readline().rstrip())
 
-    for i in range(1, len(s)):
-        mystack = []
-        mystack.append(s[:i])
+        for _ in range(n):
+            prisoners, candies, seat1 = tuple(map(int, f.readline().rstrip().split()))
+            
+            q, r = divmod(candies, prisoners)
+            if seat1 + max(0, r-1) > prisoners:
+                print(f'{(seat1 + max(0, r-1))%prisoners}') # make zero indexed
+            else:
+                print(f'{seat1 + max(0, r-1)}')
 
-        while len(''.join(mystack)) < len(s):
-            # Pop stack add 1, and then push result
-            mystack.append(str(int(mystack[-1]) + 1))
-
-        print(f'{i}, {str(int(mystack[-1]) + 1)}, {mystack}')
-        if ''.join(mystack) == s:
-            print('YES', mystack[0])
-            break
-        if i == len(s) - 1:
-            print('NO')
-
-print(separateNumbers('891011'))
+except Exception as e:
+    print(e)
