eb1746e42fe0c24ea99caffc4fc1255949514070
rrguzman1976 <35820508+rrguzman1976@users.noreply.github.com>
2018-11-04 22:33:46 -0500

    sun3

--- py-techniques/crack/sortpy.py
+++ py-techniques/crack/sortpy.py
@@ -1,14 +1,54 @@
 from random import randint
+import math
+
+# Bubble Sort: compare each item to its neighbor and swap if
+# out of order. Repeat until no swaps are necessary. Each iteration
+# will swap the largest element to the end.
+# Good for mostly sorted or small arrays O(n^2)
+def bubbleSort(arr):
+    """ In place sort """
+    j = len(arr)
+    while j > 0:
+
+        swaps = 0
+        for i in range(0, j-1):
+            if arr[i] > arr[i+1]:
+                swaps += 1
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+
+        j -= 1
+
+        if not swaps: # no more sorting needed
+            break
+
+    return arr
 
 # Good for mostly sorted or small arrays -O(n^2)
 def insertionSort(arr):
+    """ In place sort """
     L = len(arr)
     for i in range(1, L):
         j = i
         while j > 0 and arr[j] < arr[j-1]:
             arr[j], arr[j-1] = arr[j-1], arr[j]
-            print(arr)
             j -= 1
+
+    return arr
+
+# Selection Sort: find the smallest item and swap it to the
+# front of the array. Like sorting a deck of cards.
+# Good for mostly sorted or small arrays -O(n^2)
+def selectionSort(arr):
+    start = 0
+
+    while start < len(arr):
+        index, small = 0, math.inf
+        for i in range(start, len(arr)):
+            if arr[i] < small:
+                index, small = i, arr[i]
+
+        arr[start], arr[index] = arr[index], arr[start]
+        start += 1
 
     return arr
 
@@ -36,7 +76,8 @@ def mergeSortR(arr):
         else:
             merge.append(S2[pos2])
             pos2 += 1
-
+    
+    # Only 1 element difference, at most?
     if pos1 < len(S1):
         merge.extend(S1[pos1:])
     elif pos2 < len(S2):
@@ -72,4 +113,6 @@ arr = list(map(int, '10 9 8 7 6 5 4 3 2 1'.split()))
 print(arr)
 #print(mergeSortR(arr))
 #print(quickSort(arr))
-print(insertionSort(arr))
+#print(insertionSort(arr))
+#print(bubbleSort(arr))
+print(selectionSort(arr))
