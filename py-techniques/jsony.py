7b69275b7a05a1542dc766dddd3d681ca3314329
Ricardo Guzman <rrguzman1976@hotmail.com>
2018-11-07 15:20:16 -0500

    wed

--- py-techniques/jsony.py
+++ py-techniques/jsony.py
@@ -1,9 +1,14 @@
 import json
+import ijson
+from collections import defaultdict
 
-def main():
+def parseJSON(filename="C:\\Users\\ricardogu\\Desktop\\test.json"):
+
+    """ Using standard json library. Loads file into memory.
+    """
     try:
 
-        with open("C:\\Users\\ricardogu\\Desktop\\test.json", "r", encoding="utf-8") as f:
+        with open(filename, "r", encoding="utf-8") as f:
             
             jsonObj = json.load(f) # throws JSONDecodeError
 
@@ -20,6 +25,83 @@ def main():
 
     except Exception as e:
         print(e)
+
+# 
+def parseiJSON(filename="C:\\Users\\ricardogu\\Desktop\\test.json"):
+
+    """ Using ijson library allows streaming the json file.
+    """
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            
+            # Use this to get prefix for the json elements.
+            parsed = ijson.parse(f) # returns iterator
+            #print('\n'.join(list(parsed)))
+
+            server_name = server_db = None
+            ultipro, standalone = [], []
+            tmpCo = tmpAR = tmpEmpInfo = None
+
+            for pre, evt, val in parsed: # returns all (prefix, event, value)
+                if (pre, evt) == ('server_db', 'string'):
+                    server_db = val
+                elif (pre, evt) == ('server_name', 'string'):
+                    server_name = val
+                elif (pre, evt) == ('ultipro.item.ar_number', 'string'):
+                    ultipro.append(val)
+                # New standalone co
+                elif (pre, evt) == ('standalone.item', 'start_map'):
+                    tmpEmpInfo = defaultdict(int)
+                elif (pre, evt) == ('standalone.item.employee_info.item.location', 'string'):
+                    tmpEmpInfo[val] += 1
+                elif (pre, evt) == ('standalone.item.company_name', 'string'):
+                    tmpCo = val
+                elif (pre, evt) == ('standalone.item.ar_number', 'string'):
+                    tmpAR = val
+                # End standalone co
+                elif (pre, evt) == ('standalone.item', 'end_map'):
+                    if tmpEmpInfo: # ignore N/A
+                        standalone.append([tmpCo, tmpAR, tmpEmpInfo])
+
+            print(server_db)
+            print(server_name)
+            print(ultipro)
+            print(standalone)
+
+            # Get server
+            #serverdb = ijson.items(f, 'server_db') # returns iterator
+            #servername = ijson.items(f, 'server_name') # returns iterator
+
+            #print(list(serverdb)[0])
+            #print(list(servername)[0])
+
+            # Get standalone customers:
+            #alone = ijson.items(f, 'standalone.item') # returns iterator
+            
+            #for co in alone:
+            #    print(co['company_name']) # 
+            #    print(co['ar_number']) # 
+            #    print(co['employee_info']) # 
+            
+            # Get ultipro customers (only ar):
+            #arNumber = ijson.items(f, 'ultipro.item.ar_number') # returns iterator
+            
+            #for ar in arNumber:
+            #    print(ar)
+
+            ## Get ultipro customers.
+            #ultipro = ijson.items(f, 'ultipro.item') # returns iterator
+            
+            #for co in ultipro:
+            #    print(co['ar_number']) # ar no
+
+
+    except Exception as e:
+        print(e)
+
+def main():
+    #parseJSON()
+    parseiJSON()
 
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -1,8 +1,51 @@
 from collections import deque, defaultdict
 
+def weightedUniformStrings(s, queries):
+    answers, U = [], {}
+    pos = 0
+    
+    # Build U hashtable
+    while pos < len(s):
+        weight = ord(s[pos])-ord('a')+1
+        U[s[pos]] = weight
+        
+        end = pos + 1
+        while end < len(s) and s[pos] == s[end]:
+            # Add to U
+            U[s[pos:end+1]] = weight*len(s[pos:end+1])
+            end += 1
+        
+        pos = end
+    
+    print(U)
+    # Query U
+    for q in queries:
+        if q in U.values():
+            answers.append('Yes')
+        else:
+            answers.append('No')
+    
+    return answers
 
 def main():
-    pass
+    # File harness
+    try:
+
+        with open("C:\\Users\\ricardogu\\Desktop\\test.txt", "r", encoding="utf-8") as f:
+            s = f.readline().rstrip()
+            #n = int(f.readline().rstrip())
+
+            queries = []
+
+            for _ in range(int(f.readline().rstrip())):
+
+                q = int(f.readline().rstrip())
+                queries.append(q)
+
+            print(weightedUniformStrings(s, queries))
+
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
