c00e7e545197cc37abd812c01d453ef64c8e1157
rrguzman1976 <35820508+rrguzman1976@users.noreply.github.com>
2018-11-08 21:52:18 -0500

    thurs

--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -1,21 +1,24 @@
 from collections import deque, defaultdict, Counter
+from itertools import permutations
 
+def permute(s, s2):
+    st1 = deque(s)
+
+    for i in range(1, len(s) + 1):
+        st1.rotate(i)
+        tmpr = ''.join(st1)
+        print(tmpr)
+        if tmpr == s2:
+            return True
+
+    return False
 
 def main():
 
-    s1 = 'ricardozricardoz'
+    s1 = 'amazon'
+    s2 = 'zonama'
     
-    letters = Counter(s1)
-
-    for c in letters:
-
-        if letters[c] == 1:
-            print(c)
-            break
-
-    else:
-        print(None)
-    
+    print(permute(s1, s2))   
     
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
