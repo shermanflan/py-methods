60a3e93edf69eb40870dbef425662188af9b01d2
Ricardo Guzman <rrguzman1976@hotmail.com>
2018-11-27 16:35:42 -0500

    mon1

--- py-techniques/CIT-10465.py
+++ py-techniques/CIT-10465.py
@@ -5,33 +5,54 @@ from datetime import datetime
 from collections import defaultdict
 import os
 import shutil
+from pathlib import Path, PurePath
+import re
 
 # TODO: Is the shared folder on the same data center host? If not, is it faster
 # to copy the file locally first? Test this.
-def pickTopNFiles(n, src, dst):
+def pickFiles(src, arc):
     """
-    Selects the top n files by date descending.
+    Processes all files in the source and then archives.
     """
-    print(n, src, dst)
+    newfiles = []
     
     try:
+        rex = r"""
+                ^
+                .*                  # server name
+                _breach_report_
+                \d\d-\d\d           # MM-DD
+                .json
+                $
+            """
+        p = re.compile(rex, re.IGNORECASE |  re.VERBOSE)
 
         with os.scandir(path=src) as d:
             for entry in d: # os.DirEntry
-                print(entry.path)
-                print(entry.stat().st_ctime) # stat_result
-                print(datetime.fromtimestamp(entry.stat().st_ctime))
-                print(datetime.utcfromtimestamp(entry.stat().st_ctime))
+                #print(entry.path)
+                # Use pattern match to identify breach import files.
+                if entry.is_file() and p.match(entry.name):
+                    newfiles.append(entry.path)
 
-                if entry.is_file():
-                    shutil.copy2(src=entry.path, dst=dst) # preserves file metadata
+            # Process files atomically (all or nothing).
+            for fi in newfiles:
+                print(f'{fi}')
+                #parseiJSON(filename=fi)
+                parseiJSONDb(filename=fi)
 
-    except OSError as e:
-        print(e)
+            # Ok, all good -> archive.
+            for fi in newfiles:
+                #shutil.copy2(src=entry.path, dst=arc) # preserves file metadata
+                shutil.move(src=fi, dst=arc) # preserves file metadata
+
+    except Exception as e: # catch all
+        print(f'pickFiles: {e}')
+        raise
 
 def parseiJSON(filename=r"C:\Users\ricardogu\Desktop\test4.json"):
 
     """ Using ijson library enables json iteration/streaming (constant memory footprint).
+        However, keeping the object tree in memory does create additional memory overhead.
     """
     server_name = server_db = None
     ultipro, standalone = [], []
@@ -69,23 +90,101 @@ def parseiJSON(filename=r"C:\Users\ricardogu\Desktop\test4.json"):
         print(f'Parsed {len(ultipro)} ultipro, {len(standalone)} standalone customers ({elapsed})')
 
     except Exception as e:
-        print(f'Error: {e}')
+        print(f'parseiJSON: {e} for {filename}')
         return
 
     # Import data
-    dbLoad(server_db, server_name, ultipro, standalone)
+    dbLoad(server_db, server_name, ultipro, standalone, filename)
+
+def parseiJSONDb(filename=r"C:\Users\ricardogu\Desktop\test4.json"):
+
+    """ Using ijson library enables json iteration/streaming (constant memory footprint).
+        This implmentation loads to the database as it iterates ensuring a constant memory
+        footprint.
+    """
+
+    # Pass as parameters to defend against SQL injection. Also, supports plan reuse.
+    cnxnstr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=ScratchDB;Trusted_Connection=yes;'
+    qryUlti = '''
+        INSERT INTO dbo.BreachImpact(AR__c, Source, Created, CreatedBy)
+        VALUES(?, ?, ?, SYSTEM_USER)
+    '''
+    qryLegacy = '''
+        INSERT INTO dbo.BreachImpact(AR__c, company_name, location, EECount, Source, Created, CreatedBy)
+        VALUES(?, ?, ?, ?, ?, ?, SYSTEM_USER)
+    '''
+    qryMeta = '''
+        UPDATE dbo.BreachImpact
+            SET server_name = ?
+                , server_db = ?
+        WHERE   Source = ?
+    '''
+    server_name = server_db = None
+    ultipro = standalone = 0
+    tmpCo = tmpAR = tmpEmpInfo = None
+    created = datetime.now()
+    p = PurePath(filename).name
+
+    try:
+        cnxn = pyodbc.connect(cnxnstr)
+        cursor = cnxn.cursor()
+
+        with open(filename, "r", encoding="utf-8") as f:
+            
+            # Use this to get prefix for the json elements.
+            parsed = ijson.parse(f) # returns iterator
+
+            for pre, evt, val in parsed: # returns all (prefix, event, value)
+                if (pre, evt) == ('server_db', 'string'):
+                    server_db = val
+                elif (pre, evt) == ('server_name', 'string'):
+                    server_name = val
+                elif (pre, evt) == ('ultipro.item.ar_number', 'string'):
+                    ultipro += 1
+                    cursor.execute(qryUlti, val, p, created)
+                # New standalone co
+                elif (pre, evt) == ('standalone.item', 'start_map'):
+                    tmpEmpInfo = defaultdict(int)
+                elif (pre, evt) == ('standalone.item.employee_info.item.location', 'string'):
+                    tmpEmpInfo[val] += 1
+                elif (pre, evt) == ('standalone.item.company_name', 'string'):
+                    tmpCo = val
+                elif (pre, evt) == ('standalone.item.ar_number', 'string'):
+                    tmpAR = val
+                # End standalone co
+                elif (pre, evt) == ('standalone.item', 'end_map'):
+                    if tmpEmpInfo: # ignore N/A
+                        standalone += 1
+                        for loc in tmpEmpInfo.keys():
+                            cursor.execute(qryLegacy, tmpAR, tmpCo, loc, tmpEmpInfo[loc], p, created)
+
+            cursor.execute(qryMeta, server_name, server_db, p)
+
+        cnxn.commit()
+        cursor.close()
+        cnxn.close() # rolls back any uncommitted changes
+
+        elapsed = datetime.now() - created
+        print(f'Parsed {ultipro} ultipro, {standalone} standalone customers ({elapsed})')
+
+    except Exception as e:
+        # As long as your connection and cursors are not used outside the current function, 
+        # they will be closed automatically and immediatley when the function exits.
+        print(f'parseiJSONDb: {e} for {filename}')
+        raise
 
 # TODO: If loads are too slow, replace this with a CSV export + BULK import.
-def dbLoad(server_db, server_name, ultipro, standalone):
+def dbLoad(server_db, server_name, ultipro, standalone, filename):
 
     ''' Load to db
     '''
 
     cnxnstr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=ScratchDB;Trusted_Connection=yes;'
     qry = '''
-        INSERT INTO dbo.BreachImpact(AR__c, company_name, server_name, server_db, location, EECount, Created, CreatedBy)
-        VALUES(?, ?, ?, ?, ?, ?, ?, SYSTEM_USER)
+        INSERT INTO dbo.BreachImpact(AR__c, company_name, server_name, server_db, location, EECount, Source, Created, CreatedBy)
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, SYSTEM_USER)
     '''
+    p = PurePath(filename).name
 
     try:
         # TODO: Which is faster?
@@ -102,13 +201,13 @@ def dbLoad(server_db, server_name, ultipro, standalone):
     
         # Pass as parameters to defend against SQL injection. Also, supports plan reuse.
         for ar in ultipro:
-            cursor.execute(qry, ar, None, server_name, server_db, None, 0, created)
+            cursor.execute(qry, ar, None, server_name, server_db, None, 0, p, created)
         
         socounts = 0
         for co in standalone:
             for loc in co[2].keys():
                 #print(loc, len(loc))
-                cursor.execute(qry, co[1], co[0], server_name, server_db, loc, co[2][loc], created)
+                cursor.execute(qry, co[1], co[0], server_name, server_db, loc, co[2][loc], p, created)
                 socounts += 1
         
         cnxn.commit()
@@ -121,26 +220,25 @@ def dbLoad(server_db, server_name, ultipro, standalone):
     except Exception as e:
         # As long as your connection and cursors are not used outside the current function, 
         # they will be closed automatically and immediatley when the function exits.
-
         print(f'Error: {e}')
-        return
+        raise
 
-#
+# Entry point
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--srcdir', help='Specifies the source directory containing import files')
-    parser.add_argument('--dstdir', help='Specifies the target directory for placing import files')
+    parser.add_argument('--arcdir', help='Specifies the archive directory for imported files')
     parser.add_argument('--file', help='Specifies the json file to import')
     parser.add_argument('--db', help='Specifies the database connection string')
     args = parser.parse_args()
 
     if args.file:
         #if args.db:
-        parseiJSON(filename=args.file)
-    elif args.srcdir and args.dstdir:
-
-        pickTopNFiles(1, src=args.srcdir, dst=args.dstdir)
+        parseiJSONDb(filename=args.file)
+        #parseiJSON(filename=args.file)
+    elif args.srcdir and args.arcdir:
+        pickFiles(src=args.srcdir, arc=args.arcdir)
 
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
