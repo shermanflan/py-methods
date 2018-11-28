4f3006bd4560ff171ff0cc2cf140357d12f8a4d7
Ricardo Guzman <rrguzman1976@hotmail.com>
2018-11-28 16:01:17 -0500

    wed

--- py-techniques/CIT-10465.py
+++ py-techniques/CIT-10465.py
@@ -7,6 +7,23 @@ import os
 import shutil
 from pathlib import Path, PurePath
 import re
+import logging.config
+
+# Entry point
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--srcdir', help='Specifies the source directory containing import files')
+    parser.add_argument('--arcdir', help='Specifies the archive directory for imported files')
+    parser.add_argument('--file', help='Specifies the json file to import')
+    parser.add_argument('--db', help='Specifies the database connection string')
+    args = parser.parse_args()
+
+    if args.file:
+        #if args.db:
+        parseiJSONDb(filename=args.file)
+    elif args.srcdir and args.arcdir:
+        pickFiles(src=args.srcdir, arc=args.arcdir)
 
 # TODO: Is the shared folder on the same data center host? If not, is it faster
 # to copy the file locally first? Test this.
@@ -14,6 +31,7 @@ def pickFiles(src, arc):
     """
     Processes all files in the source and then archives.
     """
+    logging.config.dictConfig(getLogConfig())
     newfiles = []
     
     try:
@@ -25,76 +43,31 @@ def pickFiles(src, arc):
                 .json
                 $
             """
-        p = re.compile(rex, re.IGNORECASE |  re.VERBOSE)
+        p = re.compile(rex, re.IGNORECASE | re.VERBOSE)
 
         with os.scandir(path=src) as d:
             for entry in d: # os.DirEntry
-                #print(entry.path)
                 # Use pattern match to identify breach import files.
                 if entry.is_file() and p.match(entry.name):
                     newfiles.append(entry.path)
+            
+            if not newfiles:
+                logging.error('No files available to import!')
+                raise Exception('No files available to import!')
 
             # Process files atomically (all or nothing).
             for fi in newfiles:
-                print(f'{fi}')
-                #parseiJSON(filename=fi)
                 parseiJSONDb(filename=fi)
 
             # Ok, all good -> archive.
             for fi in newfiles:
-                #shutil.copy2(src=entry.path, dst=arc) # preserves file metadata
-                shutil.move(src=fi, dst=arc) # preserves file metadata
+                shutil.move(src=fi, dst=os.path.join(arc, entry.name)) # preserves file metadata
 
     except Exception as e: # catch all
-        print(f'pickFiles: {e}')
+        logging.error(f'pickFiles: {e}')
         raise
 
-def parseiJSON(filename=r"C:\Users\ricardogu\Desktop\test4.json"):
-
-    """ Using ijson library enables json iteration/streaming (constant memory footprint).
-        However, keeping the object tree in memory does create additional memory overhead.
-    """
-    server_name = server_db = None
-    ultipro, standalone = [], []
-    tmpCo = tmpAR = tmpEmpInfo = None
-    created = datetime.now()
-
-    try:
-        with open(filename, "r", encoding="utf-8") as f:
-            
-            # Use this to get prefix for the json elements.
-            parsed = ijson.parse(f) # returns iterator
-
-            for pre, evt, val in parsed: # returns all (prefix, event, value)
-                if (pre, evt) == ('server_db', 'string'):
-                    server_db = val
-                elif (pre, evt) == ('server_name', 'string'):
-                    server_name = val
-                elif (pre, evt) == ('ultipro.item.ar_number', 'string'):
-                    ultipro.append(val)
-                # New standalone co
-                elif (pre, evt) == ('standalone.item', 'start_map'):
-                    tmpEmpInfo = defaultdict(int)
-                elif (pre, evt) == ('standalone.item.employee_info.item.location', 'string'):
-                    tmpEmpInfo[val] += 1
-                elif (pre, evt) == ('standalone.item.company_name', 'string'):
-                    tmpCo = val
-                elif (pre, evt) == ('standalone.item.ar_number', 'string'):
-                    tmpAR = val
-                # End standalone co
-                elif (pre, evt) == ('standalone.item', 'end_map'):
-                    if tmpEmpInfo: # ignore N/A
-                        standalone.append([tmpCo, tmpAR, tmpEmpInfo])
-
-        elapsed = datetime.now() - created
-        print(f'Parsed {len(ultipro)} ultipro, {len(standalone)} standalone customers ({elapsed})')
-
-    except Exception as e:
-        print(f'parseiJSON: {e} for {filename}')
-        return
-
-    # Import data
-    dbLoad(server_db, server_name, ultipro, standalone, filename)
+    logging.shutdown()
 
 def parseiJSONDb(filename=r"C:\Users\ricardogu\Desktop\test4.json"):
 
@@ -165,13 +138,88 @@ def parseiJSONDb(filename=r"C:\Users\ricardogu\Desktop\test4.json"):
         cnxn.close() # rolls back any uncommitted changes
 
         elapsed = datetime.now() - created
-        print(f'Parsed {ultipro} ultipro, {standalone} standalone customers ({elapsed})')
+        logging.info(f'Parsed {ultipro} ultipro, {standalone} standalone customers ({elapsed})')
 
     except Exception as e:
         # As long as your connection and cursors are not used outside the current function, 
         # they will be closed automatically and immediatley when the function exits.
-        print(f'parseiJSONDb: {e} for {filename}')
+        logging.error(f'parseiJSONDb: {e} for {filename}')
         raise
+
+def getLogConfig():
+    return {
+                'version': 1,
+                'disable_existing_loggers': False,
+                'formatters': { 
+                    'standard': { 
+                        'format': '%(asctime)s [%(levelname)s]: %(message)s'
+                    }
+                },
+                'handlers': { 
+                    'default': { 
+                        'level': 'INFO',
+                        'formatter': 'standard',
+                        'class': 'logging.handlers.RotatingFileHandler',
+                        'filename': 'CIT-10465.log',
+                        'maxBytes': 1024*1024,
+                        'backupCount': 3
+                    }
+                },
+                'loggers': { 
+                    '': { 
+                        'handlers': ['default'],
+                        'level': 'INFO',
+                        'propagate': True
+                    }
+                }
+            }
+
+def parseiJSON(filename=r"C:\Users\ricardogu\Desktop\test4.json"):
+
+    """ Using ijson library enables json iteration/streaming (constant memory footprint).
+        However, keeping the object tree in memory does create additional memory overhead.
+    """
+    server_name = server_db = None
+    ultipro, standalone = [], []
+    tmpCo = tmpAR = tmpEmpInfo = None
+    created = datetime.now()
+
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            
+            # Use this to get prefix for the json elements.
+            parsed = ijson.parse(f) # returns iterator
+
+            for pre, evt, val in parsed: # returns all (prefix, event, value)
+                if (pre, evt) == ('server_db', 'string'):
+                    server_db = val
+                elif (pre, evt) == ('server_name', 'string'):
+                    server_name = val
+                elif (pre, evt) == ('ultipro.item.ar_number', 'string'):
+                    ultipro.append(val)
+                # New standalone co
+                elif (pre, evt) == ('standalone.item', 'start_map'):
+                    tmpEmpInfo = defaultdict(int)
+                elif (pre, evt) == ('standalone.item.employee_info.item.location', 'string'):
+                    tmpEmpInfo[val] += 1
+                elif (pre, evt) == ('standalone.item.company_name', 'string'):
+                    tmpCo = val
+                elif (pre, evt) == ('standalone.item.ar_number', 'string'):
+                    tmpAR = val
+                # End standalone co
+                elif (pre, evt) == ('standalone.item', 'end_map'):
+                    if tmpEmpInfo: # ignore N/A
+                        standalone.append([tmpCo, tmpAR, tmpEmpInfo])
+
+        elapsed = datetime.now() - created
+        logging.info(f'Parsed {len(ultipro)} ultipro, {len(standalone)} standalone customers ({elapsed})')
+
+    except Exception as e:
+        logging.error(f'parseiJSON: {e} for {filename}')
+        return
+
+    # Import data
+    dbLoad(server_db, server_name, ultipro, standalone, filename)
 
 # TODO: If loads are too slow, replace this with a CSV export + BULK import.
 def dbLoad(server_db, server_name, ultipro, standalone, filename):
@@ -215,32 +263,15 @@ def dbLoad(server_db, server_name, ultipro, standalone, filename):
         cnxn.close() # rolls back any uncommitted changes
         
         elapsed = datetime.now() - created
-        print(f'Loaded {len(ultipro)} ultipro customers, {socounts} standalone location counts ({elapsed})')
+        logging.info(f'Loaded {len(ultipro)} ultipro customers, {socounts} standalone location counts ({elapsed})')
 
     except Exception as e:
         # As long as your connection and cursors are not used outside the current function, 
         # they will be closed automatically and immediatley when the function exits.
-        print(f'Error: {e}')
+        logging.error(f'Error: {e}')
         raise
 
-# Entry point
-def main():
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--srcdir', help='Specifies the source directory containing import files')
-    parser.add_argument('--arcdir', help='Specifies the archive directory for imported files')
-    parser.add_argument('--file', help='Specifies the json file to import')
-    parser.add_argument('--db', help='Specifies the database connection string')
-    args = parser.parse_args()
-
-    if args.file:
-        #if args.db:
-        parseiJSONDb(filename=args.file)
-        #parseiJSON(filename=args.file)
-    elif args.srcdir and args.arcdir:
-        pickFiles(src=args.srcdir, arc=args.arcdir)
-
+# Execute only if run as the entry point into the program
 if __name__ == '__main__':
-    # execute only if run as the entry point into the program
     main()
 
