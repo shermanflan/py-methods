05ef752c3cc4e9d6859000d512bb3470d65d61f5
rrguzman1976 <35820508+rrguzman1976@users.noreply.github.com>
2018-12-15 20:15:30 -0500

    sat2

--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -8,7 +8,19 @@ import os.path
 def main():
     """
     """
-    pass
+    S = set([9, 5, 3, 4, 100, 1])
+    x = 10
+    seen = set()
+
+    for n in S:
+        if x - n in seen:
+            print(True, n, x-n)
+            break
+        
+        seen.add(n)
+    else:
+        print(False)
+                 
 
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
--- py-techniques/skinny/sort/heapsort.py
+++ py-techniques/skinny/sort/heapsort.py
@@ -1,6 +1,17 @@
+from dstruct.heap import minheap
+
 #
 # Heapsort is an implementation of selection sort using the priority queue data structure.
-# O(n log n)
+# O(n log n), stable
 #
+def heapsort(l): # inplace
+    minq = minheap(l) # O(n log n)
+
+    for i in range(len(l)):
+        l[i] = minq.getmin() # O(log n)
 
 
+#if __name__ == 'main':
+l = [5, 4, 3, 2, 1]
+heapsort(l)
+print(l)
