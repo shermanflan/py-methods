eaa5adff5982285e5e0e8531c0f5b9d326682ca5
rrguzman1976 <35820508+rrguzman1976@users.noreply.github.com>
2019-01-06 09:34:41 -0500

    Update scratch2.py

--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -1,9 +1,24 @@
+import math
 
-def main():
-    """
-    """
-    pass
+def knapsack(nums, target):
+
+    def ksshelp(i, T):
+
+        if T == 0:
+            return True
+
+        if i < 0 or T < 0:
+            return False
+
+         # i in set
+         # i not in set
+
+        return ksshelp(i - 1, T - nums[i]) or ksshelp(i - 1, T)
+
+    
+    
+    return ksshelp(len(nums) - 1, target)
 
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
-    main()
+    print(knapsack([1, 2, 5, 9, 10], 7))
