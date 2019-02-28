05e3e2d040c50df7f3d2f70131a312154621bb37
rrguzman1976 <35820508+rrguzman1976@users.noreply.github.com>
2019-02-28 18:57:13 -0500

    Update scratch2.py

--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -1,6 +1,34 @@
 import math
 from collections import deque
 
+# O (n^2)
+def climbingLeaderboard(scores, alice):
+    output = []
+
+    # 1. For each of Alice's marks
+    for mark in alice: # O(n)
+        # 2. Add Alice's mark to scores array
+        scores_copy = scores[:]
+        scores_copy.append(mark)
+
+        # 3. Convert to Set
+        scores_set = set(scores_copy)
+
+        # 4. Sort array
+        sorted_marks = sorted(list(scores_set), reverse=True)
+
+        # 5. Lookup Alice's position in the array and return it.
+        # O(n)
+        alice_rank = [i+1 for i, p in enumerate(sorted_marks) if p == mark]
+        output.append(alice_rank[0])
+
+    return output
 
 if __name__ == '__main__':
-    pass
+    scores = [100, 90, 90, 80, 75, 60]
+    alice = [50, 65, 77, 90, 102]
+
+    result = climbingLeaderboard(scores, alice)
+
+    print(result)
+
