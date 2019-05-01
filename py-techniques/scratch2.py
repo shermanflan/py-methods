32ecc3c312f3f864fb6fa167bdebf956dc5eceb1
rrguzman1976 <35820508+rrguzman1976@users.noreply.github.com>
2019-05-01 16:30:50 -0500

    Test3 from Windows

--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -3,7 +3,7 @@ from collections import deque
 #import pandas as pd
 #import numpy as np
 
-# TEST2 from Ubuntu
+# TEST3 from Window
 if __name__ == '__main__':
     arr = 'Test commit'#np.arange(9).reshape((3, 3))
     print(arr)
