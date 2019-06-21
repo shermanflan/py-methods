dffd2d13bf854d90bb5effa8534fb1f61a39b945
rrguzman1976 <35820508+rrguzman1976@users.noreply.github.com>
2019-06-21 16:26:34 -0500

    Update scratch2.py

--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -1,9 +1,6 @@
 import math
 from collections import deque
-#import pandas as pd
-#import numpy as np
 
-# TEST3 from Window
 if __name__ == '__main__':
-    arr = 'Test commit'#np.arange(9).reshape((3, 3))
+    arr = 'Test commit'
     print(arr)
