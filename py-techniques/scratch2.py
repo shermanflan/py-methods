3cd7559a45e4a91738ce0835dbf425f4d0a74e2c
rrguzman1976 <shermanflan@gmail.com>
2019-11-25 15:00:20 -0600

    new update

--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -1,6 +1,20 @@
 import math
 from collections import deque
 
+def score(dice): 
+  sum = 0
+  counter = [0,0,0,0,0,0]
+  points = [1000, 200, 300, 400, 500, 600]
+  extra = [100,0,0,0,50,0]
+  for die in dice: 
+    counter[die-1] += 1
+  
+  for (i, count) in enumerate(counter):
+    sum += (points[i] if count >= 3 else 0) + extra[i] * (count%3)
+
+  return sum 
+
 if __name__ == '__main__':
-    arr = 'Test commit'
-    print(arr)
+
+    scores=[1, 1, 1, 1, 1, 1]
+    print(score(scores))
