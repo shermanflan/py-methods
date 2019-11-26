c9a7ab2dabeee4e93c1ae0f51e08da82c12fd5b3
rrguzman1976 <shermanflan@gmail.com>
2019-11-26 11:56:01 -0600

    Cleanup

--- py-techniques/crack/heapy.py
+++
@@ -1,5 +0,0 @@
-# To Do: Implement priority queue as a min heap
-# See heapq module for standard python implmentation.
-# https://docs.python.org/3.7/library/heapq.html?highlight=heap#module-heapq
-
-
--- py-techniques/scratch2.py
+++ py-techniques/scratch2.py
@@ -1,20 +1,82 @@
 import math
 from collections import deque
 
-def score(dice): 
-  sum = 0
-  counter = [0,0,0,0,0,0]
-  points = [1000, 200, 300, 400, 500, 600]
-  extra = [100,0,0,0,50,0]
-  for die in dice: 
-    counter[die-1] += 1
-  
-  for (i, count) in enumerate(counter):
-    sum += (points[i] if count >= 3 else 0) + extra[i] * (count%3)
+"""
+Write a function that returns the elements on odd positions (0 based) in a list
+"""
+def solution1(input):
+  # Using slicer
+  #output = input[1::2]
 
-  return sum 
+  # Using iteration
+  output = [v for i, v in enumerate(input) if i%2 == 1]
+
+  return output
+
+
+"""
+Write a function that returns the cumulative sum of elements in a list
+"""
+def solution2(input):
+    # Using built in itertools.accumulate
+    runsum, tmp = [], 0
+
+    for n in input:
+      tmp += n
+      runsum.append(tmp)  
+
+    return runsum
+
+"""
+Write a function that takes a number and returns a list of its digits
+"""
+def solution3(input):
+  # Code goes here
+  output = []
+  tmp = input
+  
+  while tmp > 0:
+    
+    digit = tmp%10
+    output.append(digit)
+    tmp //= 10
+
+  return output[::-1]
+
+"""
+From: http://codingbat.com/prob/p126968
+Return the "centered" average of an array of ints, which we'll say is 
+the mean average of the values, except ignoring the largest and 
+smallest values in the array. If there are multiple copies of the 
+smallest value, ignore just one copy, and likewise for the largest 
+value. Use int division to produce the final average. You may assume 
+that the array is length 3 or more.
+"""
+
+def solution4(input):
+  # Code goes here
+  from functools import reduce
+
+  # Sort input array - min and max on opposite ends
+  in_order = sorted(input)
+  # take the slice of array ignore first and last
+  center = in_order[1:-1]
+  # return average of this list
+
+  return sum(center)//len(center) 
+
 
 if __name__ == '__main__':
 
-    scores=[1, 1, 1, 1, 1, 1]
-    print(score(scores))
+  assert solution1([0,1,2,3,4,5]) == [1,3,5]
+  assert solution1([1,-1,2,-2]) == [-1,-2]
+
+  assert solution2([1,1,1]) == [1,2,3]
+  assert solution2([1,-1,3]) == [1,0,3]
+
+  assert solution3(123) == [1,2,3]
+  assert solution3(400) == [4,0,0]
+
+  assert solution4([1, 2, 3, 4, 100]) == 3
+  assert solution4([1, 1, 5, 5, 10, 8, 7]) == 5
+  assert solution4([-10, -4, -2, -4, -2, 0]) == -3
