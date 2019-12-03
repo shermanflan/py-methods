3afa1158426bc531d230d37621d87fbca53cf0e1
rrguzman1976 <shermanflan@gmail.com>
2019-12-03 01:09:25 -0600

    Home sync

--- py-techniques/scratch1.py
+++ py-techniques/scratch1.py
@@ -1,20 +1,54 @@
-def compress(s):
-    """AABBCC|AAABCCDDDD"""
-    import re
+def move_cars(arr):
+    """
+    Assumes empty space is in pos 0
+    """
+    #print(f"BEFORE: {arr}")
+    empty_ptr, pos = 0, 1
 
-    nn2 = r"""(?P<twoplus>(?P<single>[a-zA-Z])(?P=single){1,})"""
-    re_nn2 = re.compile(nn2, flags=0)
-    compressed = re_nn2.sub(lambda m: m.group("single")+str(len(m.group("twoplus"))), s)
+    while pos < len(arr):
+        val = arr[pos]
 
-    return compressed if len(compressed) < len(s) else s
+        if pos != val:
+            # free up pos and reset ptr
+            arr[empty_ptr], arr[val] = arr[val], arr[empty_ptr]
+
+            # move val into correct pos
+            arr[val], arr[pos] = arr[pos], arr[val]
+            empty_ptr = pos
+        
+        pos += 1
+
+    pos = len(arr)-1
+
+    while pos >= 0:
+
+        val = arr[pos]
+
+        if pos != val:
+            # free up pos and reset ptr
+            arr[empty_ptr], arr[val] = arr[val], arr[empty_ptr]
+
+            # move val into correct pos
+            arr[val], arr[pos] = arr[pos], arr[val]
+            empty_ptr = pos
+        
+        pos -= 1
+
+    # Switch empty pointer
+    arr[0], arr[empty_ptr] = arr[empty_ptr], arr[0]
+    #print(f"AFTER: {arr}")
+
+    return arr
 
 if __name__ == "__main__":
 
-    t3 = 'AABBCC'
-    arr = compress(t3)
-    assert arr == 'AABBCC'
+    result = move_cars([0, 4, 1, 3, 2])
+    assert result == [0, 1, 2, 3, 4], "Simple case"
 
-    t4 = 'AAABCCDDDD'
-    arr = compress(t4)
-    assert arr == 'A3BC2D4'
+    result = move_cars([0, 1, 3, 2])
+    assert result == [0, 1, 2, 3], "Simple case 2"
 
+    result = move_cars([0, 5, 6, 7, 4, 1, 3, 2])
+    assert result == [0, 1, 2, 3, 4, 5, 6, 7], "Simple case"
+
+    print("Success")
