8f81f01058a4d830cce540633a8527e1130cae5b
rrguzman1976 <shermanflan@gmail.com>
2019-12-04 02:08:17 -0600

    Home sync

--- py-techniques/scratch1.py
+++ py-techniques/scratch1.py
@@ -1,54 +1,26 @@
-def move_cars(arr):
-    """
-    Assumes empty space is in pos 0
-    """
-    #print(f"BEFORE: {arr}")
-    empty_ptr, pos = 0, 1
-
-    while pos < len(arr):
-        val = arr[pos]
-
-        if pos != val:
-            # free up pos and reset ptr
-            arr[empty_ptr], arr[val] = arr[val], arr[empty_ptr]
-
-            # move val into correct pos
-            arr[val], arr[pos] = arr[pos], arr[val]
-            empty_ptr = pos
+def sort_the_files(n, result):
+    # Write your solution here
+    def rec(n, level, filename, files):
+        if len(files) == 1000 or filename >= n:
+            return
         
-        pos += 1
-
-    pos = len(arr)-1
-
-    while pos >= 0:
-
-        val = arr[pos]
-
-        if pos != val:
-            # free up pos and reset ptr
-            arr[empty_ptr], arr[val] = arr[val], arr[empty_ptr]
-
-            # move val into correct pos
-            arr[val], arr[pos] = arr[pos], arr[val]
-            empty_ptr = pos
+        if level > 0:
+            files.append("IMG{0}.jpg".format(filename))
         
-        pos -= 1
-
-    # Switch empty pointer
-    arr[0], arr[empty_ptr] = arr[empty_ptr], arr[0]
-    #print(f"AFTER: {arr}")
-
-    return arr
+        for digit in range(10):
+            if digit == 0 and level == 0:
+                continue
+            rec(n, level+1, filename*10+digit, files)
+    
+        return
+    
+    rec(n, 0, 0, result)
+    
+    return '\n'.join(result)
 
 if __name__ == "__main__":
 
-    result = move_cars([0, 4, 1, 3, 2])
-    assert result == [0, 1, 2, 3, 4], "Simple case"
-
-    result = move_cars([0, 1, 3, 2])
-    assert result == [0, 1, 2, 3], "Simple case 2"
-
-    result = move_cars([0, 5, 6, 7, 4, 1, 3, 2])
-    assert result == [0, 1, 2, 3, 4, 5, 6, 7], "Simple case"
-
-    print("Success")
+    files = []
+    result = sort_the_files(100, files)
+    print(f"{len(files)}")
+    print(f"{result}")
