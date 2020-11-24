7067bb9ed6c0959725fd7912a1e6c56c368286bb
shermanflan <shermanflan@gmail.com>
2020-11-24 13:25:47 -0600

    Refactored

--- soap/main.py
+++ soap/main.py
@@ -1,208 +1,61 @@
-"""
-References:
-- https://docs.python-zeep.org/en/master/index.html
-
-Miscellaneous:
-- See this [post](https://stackoverflow.com/questions/48655638/python-zeep-send-un-escaped-xml-as-content)
-for special handling re: XML escaping
-"""
 import logging
-import os
-from time import sleep
-from xml import etree
-from zipfile import ZipFile
 
-from requests import Session
-from requests.auth import HTTPBasicAuth
-from zeep import Client, Settings, Plugin
-from zeep.cache import SqliteCache
-from zeep.transports import Transport
-
-from client import (
-    ERP_URI, SOAP_URI, FUSION_USER, FUSION_USER_PWD, JOB_PACKAGE,
-    JOB_DEFINITION)
-log_level_code = getattr(logging, os.environ.get('LOG_LEVEL', ''), logging.DEBUG)
-logging.basicConfig(
-    format='%(asctime)s %(levelname)s [%(name)s]: %(message)s'
-    , datefmt='%Y-%m-%d %I:%M:%S %p'
-    , level=log_level_code)
+import utility.logging
+from utility import JOB_PACKAGE, JOB_DEFINITION
+from utility.api.oracle import OracleFusion
 
 logger = logging.getLogger(__name__)
 
 
-def submit_ess_job(client, tmp_folder, package, job):
-    request_id = client.service.submitESSJobRequest(
-        jobPackageName=package,
-        jobDefinitionName=job)
-
-    logger.info(f"Checking job status for job {request_id}")
-
-    status = client.service.getESSJobStatus(requestId=request_id)
-
-    while status != 'SUCCEEDED':
-        logger.info(f"Job status is {status}")
-        status = client.service.getESSJobStatus(requestId=request_id)
-        sleep(secs=60)
-
-    logger.info(f"Downloading job details for job {request_id}")
-
-    # Returns ns5:DocumentDetails[]
-    # ns5: DocumentDetails(
-    #     Content: ns6: base64Binary - DataHandler,
-    #     FileName: xsd:string,
-    #     ContentType: xsd:string,
-    #     DocumentTitle: xsd:string,
-    #     DocumentAuthor: xsd:string,
-    #     DocumentSecurityGroup: xsd:string,
-    #     DocumentAccount: xsd:string,
-    #     DocumentName: xsd:string,
-    #     DocumentId: xsd:string
-    # )
-    pack = client.service.downloadESSJobExecutionDetails(
-        requestId=request_id,
-        fileType="all")
-
-    for a in pack:
-        logger.debug(f"Attachment: {a.DocumentId}, {a.DocumentName}, {a.DocumentTitle}, {a.ContentType}")
-
-        tmp_zip = os.path.join(tmp_folder, a.DocumentName)
-
-        with open(tmp_zip, 'wb') as f:
-            f.write(a.Content)
-
-        with ZipFile(tmp_zip) as z:
-            z.extractall(path=tmp_folder)
-
-    return request_id
-
-
-def get_document_id(soap_client, search_query):
-    # factory = soap_client.type_factory('ns0')
-    field_element = soap_client.get_element(name='ns0:Field')
-    service_type = soap_client.get_type(name='ns0:Service')
-    search_element = field_element(search_query, name='QueryText')
-
-    search_service = service_type(
-        Document={
-            "Field": [
-                search_element
-            ]
-        },
-        IdcService='GET_SEARCH_RESULTS')
-
-    logger.info(f"field: {search_element}")
-    logger.info(f"service: {search_service}")
-
-    response = soap_client.service.GenericSoapOperation(
-        Service=search_service, webKey='cs')
-
-    for result in response['Service']['Document']['ResultSet']:
-        if result['name'] == 'SearchResults':
-            for row in result['Row']:
-                for field in row['Field']:
-                    if field['name'] == 'dID':
-                        return field['_value_1']
-    return None
-
-
 if __name__ == '__main__':
 
-    session = Session()
-    session.auth = HTTPBasicAuth(FUSION_USER, FUSION_USER_PWD)
+    # tmp_folder = './local/data'
+    tmp_folder = './jupyter/data'
 
-    settings = Settings(strict=True,
-                        xml_huge_tree=True,
-                        raw_response=False,
-                        force_https=True
-                        )
-    transport = Transport(cache=SqliteCache(), session=session)
+    erp = OracleFusion()
 
-    # erp_client = Client(wsdl=ERP_URI,
-    #                     settings=settings,
-    #                     transport=transport)
+    logger.info(f"Initiating {JOB_DEFINITION} job request")
 
-    # logger.info("Initiating job request")
-    #
-    # request_id = submit_ess_job(erp_client,
-    #                             tmp_folder='./jupyter/data',
-    #                             package=JOB_PACKAGE,
-    #                             job=JOB_DEFINITION)
-    #
-    # logger.info(f"Job request {request_id} complete")
+    job_id = erp.submit_ess_job_request(package=JOB_PACKAGE,
+                                        job=JOB_DEFINITION)
 
-    # logger.info(f"Searching files for {request_id} complete")
-    soap_client = Client(wsdl=SOAP_URI,
-                         settings=settings,
-                         transport=transport
-                         )
+    logger.info(f"Checking job status for job {job_id}")
 
-    search_query = u"""
-    dOriginalName <starts> `MANIFEST_DATA_41`
+    job_status = erp.get_ess_job_status(job_id)
+
+    if job_status != 'SUCCEEDED':
+        raise Exception(f"Job request for {JOB_DEFINITION} failed")
+
+    logger.info(f"Job request {job_id} complete")
+
+    logger.info(f"Downloading job details for job {job_id}")
+
+    documents_df = erp.download_ess_job_execution_details(job_id,
+                                                          tmp_folder)
+
+    logger.info(f"Downloaded {documents_df.shape[0]} documents")
+
+    file_name = 'MANIFEST_DATA_41'
+    # file_name = 'file_fscmtopmodelam_finartoppublicmodelam_transactionheaderpvo-batch1944942919-20201012_063148.zip'
+    search_query = f"""
+    dOriginalName <starts> `{file_name}`
     <AND> dSecurityGroup <starts> `OBIAImport`
     """.strip()
 
     logger.info(f"Searching files for '{search_query}'")
 
-    document_id = get_document_id(soap_client, search_query)
+    results_df = erp.get_search_results(search_query)
 
-    if document_id:
-        logger.info(f"Found document {document_id}")
+    if results_df.shape[0] > 0:
+        logger.info(f"Found {results_df.shape[0]} documents")
     else:
-        raise Exception(f"Document {document_id} does not exist")
+        raise Exception(f"No documents found")
 
-    document_id = 57296
-    logger.info(f"Downloading document {document_id}")
+    for r in results_df.itertuples(index=False):
+        logger.info(f"Downloading {r.dOriginalName} to {tmp_folder}")
 
-    field_element = soap_client.get_element(name='ns0:Field')
-    service_type = soap_client.get_type(name='ns0:Service')
-    doc_id_element = field_element(document_id, name='dID')
+        doc_df = erp.get_file(r.dID, tmp_folder)
 
-    file_service = service_type(
-        Document={
-            "Field": [
-                doc_id_element
-            ]
-        },
-        IdcService='GET_FILE')
+        logger.info(f"Downloaded {doc_df.shape[0]} documents")
 
-    logger.info(f"field: {doc_id_element}")
-    logger.info(f"service: {file_service}")
-
-    # ns0:Field(xsd:string, name: xsd:anySimpleType)
-    # ns0:ResultSet(Row: ns0:Row[], name: xsd:anySimpleType)
-    # ns0:Container(Field: ns0:Field[], ResultSet: ns0:ResultSet[], OptionList: ns0:OptionList[], _attr_1: {})
-    # ns0:Service(
-    #     User: ns0:Container,
-    #     Document: {
-    #         Field: ns0:Field[],
-    #         ResultSet: ns0:ResultSet[],
-    #         OptionList: ns0:OptionList[],
-    #         File: ns0:File[],
-    #         _attr_1: {}},
-    #     IdcService: xsd:anySimpleType)
-    # GenericSoapOperation(Service: ns0:Service, webKey: xsd:anySimpleType)
-    #     -> Service: ns0:Service, webKey: xsd:anySimpleType
-    response = soap_client.service.GenericSoapOperation(
-        Service=file_service, webKey='cs')
-
-    doc_name = None
-    # TODO: look into pandas explode, melt, etc.
-    for attach in response['Service']['Document']['ResultSet']:
-        if attach['name'] == 'FILE_DOC_INFO':
-            for row in attach['Row']:
-                for field in row['Field']:
-                    if field['name'] == 'dOriginalName':
-                        doc_name = field['_value_1']
-                        break
-
-    if doc_name:
-        tmp_folder = './jupyter/data'
-        doc_content = None
-        for attach in response['Service']['Document']['File']:
-            with open(os.path.join(tmp_folder, doc_name), 'wb') as f:
-                f.write(attach['Contents'])
-
-            # with ZipFile(tmp_zip) as z:
-            #     z.extractall(path=tmp_folder)
-
-            logger.info(f"Downloaded document {doc_name}")
+    logger.info(f"Process complete")
---
+++ soap/utility/api/oracle.py
@@ -0,0 +1,203 @@
+"""
+References:
+- https://docs.python-zeep.org/en/master/index.html
+
+Miscellaneous:
+- See this [post](https://stackoverflow.com/questions/48655638/python-zeep-send-un-escaped-xml-as-content)
+for special handling re: XML escaping
+"""
+import logging
+import os
+from time import sleep
+from zipfile import ZipFile
+
+import pandas as pd
+from requests import Session
+from requests.auth import HTTPBasicAuth
+from zeep import Client, Settings
+from zeep.cache import SqliteCache
+from zeep.transports import Transport
+
+from utility import (
+    ERP_URI, SOAP_URI, FUSION_USER, FUSION_USER_PWD
+)
+import utility.logging
+
+logger = logging.getLogger(__name__)
+
+
+class OracleFusion:
+    def __init__(self):
+        self.erp_client = None
+        self.soap_client = None
+
+    def get_soap_client(self):
+        if self.soap_client:
+            return self.soap_client
+
+        session = Session()
+        session.auth = HTTPBasicAuth(FUSION_USER, FUSION_USER_PWD)
+
+        settings = Settings(strict=True,
+                            xml_huge_tree=True,
+                            raw_response=False,
+                            force_https=True
+                            )
+        transport = Transport(cache=SqliteCache(), session=session)
+
+        logger.info("Creating ERP client")
+
+        self.soap_client = Client(wsdl=SOAP_URI,
+                             settings=settings,
+                             transport=transport
+                             )
+        return self.soap_client
+
+    def get_erp_client(self):
+        if self.erp_client:
+            return self.erp_client
+
+        session = Session()
+        session.auth = HTTPBasicAuth(FUSION_USER, FUSION_USER_PWD)
+
+        settings = Settings(strict=True,
+                            xml_huge_tree=True,
+                            raw_response=False,
+                            force_https=True
+                            )
+        transport = Transport(cache=SqliteCache(), session=session)
+
+        logger.info("Creating SOAP client")
+
+        self.erp_client = Client(wsdl=ERP_URI,
+                            settings=settings,
+                            transport=transport)
+
+        return self.erp_client
+
+    def submit_ess_job_request(self, package, job, parameters=None):
+        return self.get_erp_client().service.submitESSJobRequest(
+            jobPackageName=package,
+            jobDefinitionName=job,
+            paramList=parameters)
+
+    def get_ess_job_status(self, request_id, no_wait=False,
+                           max_tries=360,
+                           poke_interval=10):
+        status = self.get_erp_client().service.getESSJobStatus(
+            requestId=request_id)
+
+        if not no_wait:
+            # TODO: What are the completion statuses?
+            tries = 0
+            while status not in ('SUCCEEDED', 'FAILED') \
+                    and tries < max_tries:
+                logger.debug(f"Job status is {status}")
+                status = self.get_erp_client().service.getESSJobStatus(
+                    requestId=request_id)
+                sleep(poke_interval)
+                tries += 1
+
+        return status
+
+    def download_ess_job_execution_details(self, request_id,
+                                           output_folder,
+                                           file_type='log',
+                                           save_attachments=False):
+        pack = self.get_erp_client().service.downloadESSJobExecutionDetails(
+            requestId=request_id,
+            fileType=file_type)
+
+        documents = []
+        for a in pack:
+            logger.debug(f"Attachment: {a.DocumentId}, {a.DocumentName}, {a.DocumentTitle}")
+
+            documents.append({k: a[k] for k in a if k != 'Content'})
+
+            if save_attachments:
+                tmp_path = os.path.join(output_folder, a.DocumentName)
+
+                with open(tmp_path, 'wb') as f:
+                    f.write(a.Content)
+                if a.ContentType == 'zip':
+                    with ZipFile(tmp_path) as z:
+                        z.extractall(path=output_folder)
+
+        return pd.DataFrame(documents)
+
+    def get_search_results(self, query):
+        field_element = self.get_soap_client().get_element(name='ns0:Field')
+        service_type = self.get_soap_client().get_type(name='ns0:Service')
+
+        search_service = service_type(
+            Document={
+                "Field": [
+                    field_element(query, name='QueryText')
+                ]
+            },
+            IdcService='GET_SEARCH_RESULTS')
+
+        logger.debug(f"service: {search_service}")
+
+        response = self.get_soap_client().service.GenericSoapOperation(
+            Service=search_service, webKey='cs')
+
+        results = []
+        for result in response['Service']['Document']['ResultSet']:
+            if result['name'] == 'SearchResults':
+                for row in result['Row']:
+                    result = {}
+                    for field in row['Field']:
+                        result[field['name']] = field['_value_1']
+                    results.append(result)
+
+        return pd.DataFrame(results)
+
+    def get_file(self, document_id, output_folder):
+        field_element = self.get_soap_client().get_element(
+            name='ns0:Field')
+        service_type = self.get_soap_client().get_type(
+            name='ns0:Service')
+
+        file_service = service_type(
+            Document={
+                "Field": [
+                    field_element(document_id, name='dID')
+                ]
+            },
+            IdcService='GET_FILE')
+
+        logger.debug(f"service: {file_service}")
+
+        response = self.get_soap_client().service.GenericSoapOperation(
+            Service=file_service, webKey='cs')
+
+        documents = []
+        for attach in response['Service']['Document']['ResultSet']:
+            if attach['name'] == 'FILE_DOC_INFO':
+                for row in attach['Row']:
+                    document = {}
+                    for field in row['Field']:
+                        document[field['name']] = field['_value_1']
+                    documents.append(document)
+
+        docs_df = pd.DataFrame(documents)
+
+        if docs_df.shape[0] > 0:
+
+            for attach in response['Service']['Document']['File']:
+                tmp_path = os.path.join(output_folder, attach['href'])
+                with open(tmp_path, 'wb') as f:
+                    f.write(attach['Contents'])
+
+                content_type = docs_df.loc[
+                    docs_df.dOriginalName == attach['href'],
+                    'dFormat'].iloc[0]
+
+                if content_type == 'application/zip':
+                    with ZipFile(tmp_path) as z:
+                        z.extractall(path=output_folder)
+
+            return docs_df
+        else:
+            raise Exception("Document does not exist")
