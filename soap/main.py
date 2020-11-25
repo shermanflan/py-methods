cca117a2376373ae1061ff6c866ccea38ad27b0e
shermanflan <shermanflan@gmail.com>
2020-11-25 13:11:59 -0600

    Added lake routine

--- soap/main.py
+++ soap/main.py
@@ -1,17 +1,67 @@
+from datetime import datetime
+import io
+import json
 import logging
 from tempfile import TemporaryDirectory
+import uuid
 
+from azure.eventgrid import EventGridClient
+from azure.core.exceptions import (ResourceNotFoundError,
+                                   ResourceExistsError)
+from azure.storage.filedatalake import DataLakeServiceClient
+from msrest.authentication import TopicCredentials
+
+from utility import (
+    EVENT_TOPIC_URI, EVENT_TOPIC_KEY,
+    LAKE_URL, STORE_KEY,
+    LAKE_CONTAINER, LAKE_FOLDER_PATH)
+from utility.api.lake import LakeFactory
 from utility.etl import request_and_load
 
 logger = logging.getLogger(__name__)
+
+
+def publish_event():
+    credentials = TopicCredentials(EVENT_TOPIC_KEY)
+    event_grid_client = EventGridClient(credentials)
+    event_grid_client.publish_events(
+        topic_hostname=EVENT_TOPIC_URI,
+        events=[{
+            "id": uuid.uuid4(),
+            "subject": "new-job-oracle-fusion",
+            "data": {
+                "source": "bsh.aci.oracle2lake.dev",
+                "file_name_prefix": "MANIFEST_DATA_41"
+            },
+            "event_type": "new-job-event-3",
+            "event_time": datetime.utcnow(),
+            "data_version": 1
+        }]
+    )
 
 
 if __name__ == '__main__':
 
     # with TemporaryDirectory() as tmp_folder:
     # tmp_folder = './local/data'
-    tmp_folder = './jupyter/data'
+    # tmp_folder = './jupyter/data'
+    # request_and_load(output_folder=tmp_folder)
 
-    request_and_load(output_folder=tmp_folder)
+    publish_event()
+
+    # logger.info("Uploading to lake")
+
+    # Using string
+    # data = json.dumps({"key": "testing text"})
+    # LakeFactory().upload_data(lake_container=LAKE_CONTAINER, lake_dir=LAKE_FOLDER_PATH,
+    #                           file_name='test-02.txt', data=data)
+
+    # Using bytes
+    # data = io.BytesIO()
+    # num_bytes = data.write(b"some data2")
+    # data_bytes = data.getvalue()  # read()
+    #
+    # LakeFactory().upload_data(lake_container=LAKE_CONTAINER, lake_dir=LAKE_FOLDER_PATH,
+    #                           file_name='test-03.txt', data=data_bytes)
 
     logger.info(f"Process complete")
--- soap/utility/api/lake.py
+++ soap/utility/api/lake.py
@@ -81,3 +81,31 @@ class LakeFactory(object):
                 fc.flush_data(len(data))
 
                 logger.info(f'Uploaded to lake: {file} to {lake_dir}/{tail}')
+
+    def upload_data(self, lake_container, lake_dir, file_name, data):
+        """
+        Upload in-memory data to lake.
+
+        :param lake_container: lake container
+        :param lake_dir: lake target path
+        :param file_name: the target file name
+        :param data: data bytes
+        :return: None
+        """
+        lake_client = self.__get_connection()
+        fs = lake_client.get_file_system_client(file_system=lake_container)
+        dc = fs.get_directory_client(directory=lake_dir)
+
+        try:
+            dc.create_directory()
+        except ResourceExistsError as e:
+            logging.error(f'Lake container exists: {lake_container}')
+        except ResourceNotFoundError as e:
+            logging.error(f'Lake container does not exist: {lake_container}')
+            raise
+
+        fc = dc.create_file(file=file_name)
+        fc.append_data(data, offset=0, length=len(data))
+        fc.flush_data(len(data))
+
+        logging.info(f'Uploaded to lake: {lake_dir}/{file_name}')
